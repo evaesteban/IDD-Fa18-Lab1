ffe9d3f04a0aba544bc2247fabcc6a52ed254218
evaesteban <52836107+evaesteban@users.noreply.github.com>
2019-09-09 11:03:26 -0400

    Add files via upload

---
+++ Blink_Own_LED.ino
@@ -0,0 +1,22 @@
+/*
+ ECE 5413: Developing and Designing Interactive Devices 
+ Laboratory 1 Part C Exercise 2
+ Author: Eva Pilar Esteban Velasco
+ Date: 09/05/2019
+*/
+
+void setup() {
+  
+  // set PIN 9 as output.
+  pinMode(9, OUTPUT);
+  
+}
+
+void loop() {
+  
+  digitalWrite(9, HIGH);   // turn LED on
+  delay(2000);             // wait two seconds
+  digitalWrite(9, LOW);    // turn LED off 
+  delay(2000);             // wait two seconds
+  
+}
